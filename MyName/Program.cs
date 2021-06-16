7b37b4bf08502f8dedfdb7575a990b659e476c2a
Александра Паустовская <sahsa.com1997@gmail.com>
2021-06-16 11:38:30 +0300

    Добавьте файлы проекта.

---
+++ MyName/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyName
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string myName;
+            myName = "Jane";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
